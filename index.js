145327695dd06b725937054d1dea9a799015e93b
Stephen Kubovic <skubovic@gmail.com>
2018-01-04 09:40:58 -0500

    Testing RDS database connectivity

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
+const knex = require('knex')({
+  client: 'pg',
+  connection: process.env.DATABASE_URI
+})
+
 exports.handler = (event, context, callback) => {
-  const currentTime = new Date().toTimeString()
-  callback(null, {
-    statusCode: '200',
-    body: `The current time is ${currentTime}`
-  })
+  console.log('DATABASE_URI', process.env.DATABASE_URI)
+
+  knex
+    .select('*')
+    .from('pg_catalog.pg_tables')
+    .whereNotIn('schemaname', ['pg_catalog', 'information_schema'])
+    .then(rows => {
+      callback(null, {
+        statusCode: '200',
+        body: JSON.stringify(rows)
+      })
+    })
+    .catch(err => {
+      callback(null, {
+        statusCode: '500',
+        body: err.toString()
+      })
+    })
 }
